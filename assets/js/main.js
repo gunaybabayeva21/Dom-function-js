4aadf547dcec4c038cdd4f49ab6914ea424ff43d
gunaybabayeva21 <gunaybabayeva032@gmail.com>
2023-03-10 12:56:43 +0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+vdacument.creat("Table")
+dacument.appenchilde("")
+dacument.creatElement("tr")
+dacument.appenchilde(element2)
+dacument.creatElement("td")
+dacument.appenchilde(element4)
+
+const people=[{
+
+    Name:"Lale",
+    Surname:"Eliyeva",
+    Age:"20"
+},
+{
+    Name:"Ayaz",
+    Surname:"Veliyev",
+    Age:"24"
+    
+}, 
+{
+    Name:"Gunay",
+    Surname:"Babayeva",
+    Age:"21"
+    
+},   
+{   
+    Name:"Murad",
+    Surname:"Heyderov",
+    Age:"34"
+},
+]
+for(let i=0;i<people.length;i++){
+    
+}
+ cosole.log(people[i])
